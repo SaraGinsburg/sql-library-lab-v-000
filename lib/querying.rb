d19e445e59dc6e6f4225aed90969c4a0b87a4fd6
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-05-27 18:58:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,9 +53,9 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, books.series_id
    FROM characters
-   JOIN series
+   INNER JOIN series
    ON characters.series_id = series.id
-   JOIN books 
+   INNER JOIN books 
    ON books.series_id = series.id
-   ORDER BY characters.name;"
+   ;"
 end
